8f5a41638afee1cdd684b40a81dbfeb9eddefbc3
Dan Kang <dan@dskang.com>
2013-08-08 18:34:57 -0400

    creep hard

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 var express = require("express");
 var app = express();
 app.use(express.logger());
+app.use(express.bodyParser());
+
+currentUrl = null;
 
 app.get('/', function(request, response) {
-    response.send('Hello World!');
+    response.redirect(currentUrl);
+});
+
+app.post('/url', function(request, response) {
+  currentUrl = request.body.url;
+  console.log("URL: " + currentUrl);
+  response.send(200);
 });
 
 var port = process.env.PORT || 5000;
